6fed7ddca5c2d9a2b50e45059bccb85c3fee1d07
Drake Morrison <drakermorrison@gmail.com>
2018-06-27 18:40:39 -0500

    components are showing on the dom

---
+++ src/components/AnimalListings/AnimalListings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AnimalListings.css';
+
+class AnimalListings extends React.Component {
+  render () {
+    return (
+      <div className='AnimalListings'>
+        <h2>AnimalListings</h2>
+      </div>
+    );
+  }
+}
+
+export default AnimalListings;
---
+++ src/components/NewAnimalForm/NewAnimalForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewAnimalForm.css';
+
+class NewAnimalForm extends React.Component {
+  render () {
+    return (
+      <div className='NewAnimalForm'>
+        <h2>NewAnimalForm</h2>
+      </div>
+    );
+  }
+}
+
+export default NewAnimalForm;
